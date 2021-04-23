2ef0635015fd000da386be599bc3dc9772bafb8e
root <root@bitcoissue.com>
2021-04-23 09:46:54 +0900

    b

--- views/_layout/bootstrap/layout.php
+++ views/_layout/bootstrap/layout.php
@@ -53,6 +53,13 @@ var cookie_prefix = "<?php echo config_item('cookie_prefix'); ?>";
 
 
 
+
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-M5Z4BTJ');</script>
+
 <!-- Google Tag Manager -->
 <!-- <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
@@ -64,12 +71,35 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 <script type="text/javascript">
       kakaoPixel('8328026460603858780').pageView();
 </script>
+
+<!-- Facebook Pixel Code -->
+<script>
+!function(f,b,e,v,n,t,s)
+{if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+n.queue=[];t=b.createElement(e);t.async=!0;
+t.src=v;s=b.getElementsByTagName(e)[0];
+s.parentNode.insertBefore(t,s)}(window, document,'script',
+'https://connect.facebook.net/en_US/fbevents.js');
+fbq('init', '1753298861503613');
+fbq('track', 'PageView');
+</script>
+
+<!-- End Facebook Pixel Code -->
+
+
 </head>
 <body <?php echo isset($view) ? element('body_script', $view) : ''; ?>>
 <!-- Google Tag Manager (noscript) -->
 <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-M5Z4BTJ"
 height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
 <!-- End Google Tag Manager (noscript) -->
+
+<noscript><img height='1' width='1' style='display:none'
+src='https://www.facebook.com/tr?id=1753298861503613&ev=PageView&noscript=1'
+/></noscript>
+
 <div class="wrapper">
 
 	<?php if ($this->cbconfig->get_device_view_type() !== 'mobile') {} ?>
